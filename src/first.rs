2e11aa687fe87f85407baddf029feb4f0dbfb2b6
indirection42 <jiyuanz95@gmail.com>
2023-12-21 11:09:58 +0800

    feat: a bad stack

---
+++ src/first.rs
@@ -0,0 +1,77 @@
+use std::mem;
+struct Node {
+    elem: i32,
+    next: Link,
+}
+enum Link {
+    Empty,
+    More(Box<Node>),
+}
+
+struct List {
+    head: Link,
+}
+
+impl List {
+    pub fn new() -> Self {
+        Self { head: Link::Empty }
+    }
+    pub fn push(&mut self, value: i32) {
+        let node = Box::new(Node {
+            elem: value,
+            next: mem::replace(&mut self.head, Link::Empty),
+        });
+        self.head = Link::More(node);
+    }
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut cur_link = mem::replace(&mut self.head, Link::Empty);
+        while let Link::More(mut boxed_node) = cur_link {
+            cur_link = mem::replace(&mut boxed_node.next, Link::Empty);
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+
+        // Check empty list behaves right
+        assert_eq!(list.pop(), None);
+
+        // Populate list
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        // Push some more just to make sure nothing's corrupted
+        list.push(4);
+        list.push(5);
+
+        // Check normal removal
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        // Check exhaustion
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+}
